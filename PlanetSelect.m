93d5268cd9df73e3773ffcc58e5ed06a12de352f
TheGreatFez <raul.maldonado@mavs.uta.edu>
2015-08-09 23:15:25 -0500

    Fix for aerodynamic bug at 0 airspeed.
    Added thrust to analysis capability.

--- PlanetSelect.m
+++ PlanetSelect.m
@@ -1,4 +1,4 @@
-Planet = 'Mars'; % Choice between Earth and Mars
+Planet = 'Earth'; % Choice between Earth and Mars
 load 'DragData_3manCap.mat'
 load 'EarthAtmoDataRSS.mat'
 load 'Mars Atmo Data.mat'
@@ -17,23 +17,23 @@ entry_angle = 6.5; % degrees
 entry_speed = 11000; % m/s
 
 Px = 0; % m
-Py = 3600000; % m
+Py = R_planet; % m
 
 X = [Px;Py;0];
 Y = [0;0;Omega_planet];
 Z = cross(X,Y);
 
-Vx = sqrt(GM_planet/Py)-100; % m/s
-Vy = 0; % m/s
+Vx = Z(1); % m/s
+Vy = Z(2); % m/s
 
 CraftInitialV = [Vx;Vy];
 CraftInitialP = [Px;Py];
 
-RefSurfArea = 1; % m^2
-Cd = 1;
+RefSurfArea = 7.06; % m^2
+Cd = .15;
 %Mass = 5600; % kg
-WetMass = 10000; % kg
-DryMass = 0; % kg 
+WetMass = 7100; % kg
+DryMass = 3100; % kg 
 
 g0 = 9.80665; % m/s^2 standard gravity
 q0 = 1.1577; % kg/m^3
