ac3eef88e04b6a2085b9c263061102e97166f983
TheGreatFez <raul.maldonado@mavs.uta.edu>
2015-08-18 02:47:57 -0500

    Added first chart implementation for staging

--- PlanetSelect.m
+++ PlanetSelect.m
@@ -2,6 +2,7 @@ Planet = 'Earth'; % Choice between Earth and Mars
 load 'DragData_3manCap.mat'
 load 'EarthAtmoDataRSS.mat'
 load 'Mars Atmo Data.mat'
+load 'ThrustMassData.mat'
 [R_planet,Omega_planet,AtmoAlt_planet,GM_planet,Planet] = PlanetParameters(Planet);
 
 if strcmp('Earth',Planet) == 1
@@ -32,8 +33,8 @@ CraftInitialP = [Px;Py];
 RefSurfArea = 7.06; % m^2
 Cd = .15;
 %Mass = 5600; % kg
-WetMass = 7100; % kg
-DryMass = 3100; % kg 
-
+%WetMass = 7100; % kg
+%DryMass = 3100; % kg 
+LastStage = size(ThrustMassData,1);
 g0 = 9.80665; % m/s^2 standard gravity
 q0 = 1.1577; % kg/m^3
